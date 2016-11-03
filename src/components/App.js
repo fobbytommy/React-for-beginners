e54ca1834389057e10f1221d5a728ff4c065d883
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-11-02 21:05:50 -0700

    added state for AddFishForm & App components

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,29 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+	constructor() {
+		super(); // to use 'this'
+
+
+		this.addFish = this.addFish.bind(this);
+		// getInitialState()
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		// update our state
+		const fishes = {...this.state.fishes}; // ... is spread in ES6. making a copy
+		// add in our new fish
+		const timestamp = Date.now(); // adding time in millisecs
+		fishes[`fish-${timestamp}`] = fish;
+		// set state
+		this.setState({ fishes }) // same thing as { fishes: fishes }
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -11,7 +34,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={ this.addFish }/>
 			</div>
 		);
 	}
