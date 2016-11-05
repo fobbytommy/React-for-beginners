0c215c7f5ccf267919aa724482bc53695dff5d13
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-11-04 22:02:33 -0700

    'updating order state' is working.

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 
 		// getInitialState()
 		this.state = {
@@ -36,6 +37,14 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key) {
+		const order = {...this.state.order}; // object spread
+		// update or add the new number of fish orderd
+		order[key] = order[key] + 1 || 1;
+		// update our state
+		this.setState({ order })
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -45,7 +54,7 @@ class App extends React.Component {
 						{
 							Object
 								.keys(this.state.fishes)
-								.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+								.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
 						}
 					</ul>
 				</div>
