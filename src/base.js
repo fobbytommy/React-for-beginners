61b7e8992013a1cb5f7085a7ad8e82d8617de050
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-11-08 22:57:25 -0800

    connected to the firebase database for sync.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCl1IdBoZAgFN0pCJSxuOoOdwApSzW2OFs",
+	authDomain: "catch-of-the-day-tommy-oh.firebaseapp.com",
+	databaseURL: "https://catch-of-the-day-tommy-oh.firebaseio.com",
+});
+
+export default base;
